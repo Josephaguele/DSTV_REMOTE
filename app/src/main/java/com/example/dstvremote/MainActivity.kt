38415d5bde690c011707aa05d17fffd6c09494cc
Joseph <osekuemen@gmail.com>
2022-02-28 11:40:31 +0100

    hid the action bar

--- app/src/main/java/com/example/dstvremote/MainActivity.kt
+++ app/src/main/java/com/example/dstvremote/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.dstvremote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // hide action bar
+        supportActionBar?.hide()
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
     }
 }
